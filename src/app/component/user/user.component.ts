ba0123c01af92dff25b0c726310393d3ab9330ab
iggydob <138367360+iggydob@users.noreply.github.com>
2024-07-03 23:28:15 +0300

    home.component.html refactored

--- src/app/component/user/user.component.ts
+++ src/app/component/user/user.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {UserService} from "../../service/user.service";
+import {Router} from "@angular/router";
+import {DataState} from "../../enum/datastate.enum";
+import {catchError, map, of, startWith} from "rxjs";
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
 
+export class UserComponent {
+  constructor(private router: Router, userService: UserService) {
+  }
+
+  ngOnInit(): void {
+    this.homeState$ = this.customerService.customers$()
+      .pipe(
+        map(response => {
+          return {dataState: DataState.LOADED, appData: response};
+        }),
+        startWith({dataState: DataState.LOADING}),
+        catchError((error: string) => {
+          return of({dataState: DataState.ERROR, error})
+        })
+      )
+  }
+
+  selectCustomer(customer: Customer): void {
+    this.router.navigate([`/customers/${customer.id}`]);
+  }
 }
